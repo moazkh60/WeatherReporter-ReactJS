b67d2a7b973a479eb6e4642ba36cf676dc7da56c
Moaz Khan <moazkh60@gmail.com>
2018-07-19 16:47:14 +0500

    Temparate,pressure and humidity set.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,25 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Sparklines, SparklinesLine} from 'react-sparklines';
+import Chart from '../components/chart';
 
 class WeatherList extends Component{
     renderWeather(cityData){
         const name = cityData.city.name;
         const temps = cityData.list.map(weather => weather.main.temp);
-    
+        const pressure = cityData.list.map(weather => weather.main.pressure);
+        const humidity = cityData.list.map(weather => weather.main.humidity);
+
         return(
             <tr key={name}>
                 <td>{name}</td>
                 <td>
-                    <Sparklines height = {120} width={180} data={temps}>
-                    <SparklinesLine color="red"/>
-                    </Sparklines>
+                    <Chart data={temps} color="orange" unit="K"/>
+                </td>
+                <td>
+                    <Chart data={pressure} color="green" unit="hPa"/>
+                </td>
+                <td>
+                    <Chart data={humidity} color="black" unit="%"/>
                 </td>
             </tr>
         );
@@ -26,9 +32,9 @@ class WeatherList extends Component{
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
